e995e118db3e62048e25c5ee2414405f2feae34d
Daniel Boline <dboline@mediamath.com>
2019-04-29 16:37:14 -0400

    make InsertFileInfoCache owned instead of borrowed

--- src/models.rs
+++ src/models.rs
@@ -17,15 +17,15 @@ pub struct FileInfoCache {
 
 #[derive(Insertable)]
 #[table_name = "file_info_cache"]
-pub struct InsertFileInfoCache<'a> {
-    pub filename: &'a str,
-    pub filepath: Option<&'a str>,
-    pub urlname: Option<&'a str>,
-    pub md5sum: Option<&'a str>,
-    pub sha1sum: Option<&'a str>,
+pub struct InsertFileInfoCache {
+    pub filename: String,
+    pub filepath: Option<String>,
+    pub urlname: Option<String>,
+    pub md5sum: Option<String>,
+    pub sha1sum: Option<String>,
     pub filestat_st_mtime: Option<i32>,
     pub filestat_st_size: Option<i32>,
-    pub serviceid: Option<&'a str>,
-    pub servicetype: &'a str,
-    pub servicesession: Option<&'a str>,
+    pub serviceid: Option<String>,
+    pub servicetype: String,
+    pub servicesession: Option<String>,
 }
