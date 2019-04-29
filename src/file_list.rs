5e0f122a513e60c595d3bb7ec6c2d52ea26a493a
Daniel Boline <dboline@mediamath.com>
2019-04-29 16:38:21 -0400

    add cache_file_list method

--- src/file_list.rs
+++ src/file_list.rs
@@ -1,9 +1,15 @@
-use failure::Error;
+use diesel::prelude::*;
+use failure::{err_msg, Error};
+use rayon::prelude::*;
 use reqwest::Url;
 use std::path::PathBuf;
 
 use crate::file_info::{FileInfo, ServiceSession};
 use crate::file_service::FileService;
+use crate::map_result_vec;
+use crate::models::FileInfoCache;
+use crate::pgpool::PgPool;
+use crate::schema::file_info_cache;
 
 pub struct FileListConf {
     pub basedir: PathBuf,
@@ -19,4 +25,24 @@ pub struct FileList {
 
 pub trait FileListTrait {
     fn fill_file_list(conf: FileListConf) -> Result<FileList, Error>;
+}
+
+pub fn cache_file_list(pool: &PgPool, flist: &FileList) -> Result<Vec<FileInfoCache>, Error> {
+    let conn = pool.get()?;
+
+    let flist_cache: Vec<Result<_, Error>> = flist
+        .filelist
+        .par_iter()
+        .map(|f| {
+            let finfo_cache = f.get_cache_info()?;
+            Ok(finfo_cache)
+        })
+        .collect();
+
+    let flist_cache = map_result_vec(flist_cache)?;
+
+    diesel::insert_into(file_info_cache::table)
+        .values(&flist_cache)
+        .get_results(&conn)
+        .map_err(err_msg)
 }
