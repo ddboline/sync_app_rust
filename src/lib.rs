147a692b4f35ba6fdb541645d2bab4eaf4d53fa0
Daniel Boline <dboline@mediamath.com>
2019-04-29 16:37:29 -0400

    map_result_vec

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,23 @@ pub mod file_list;
 pub mod file_list_local;
 pub mod file_service;
 pub mod models;
+pub mod pgpool;
 pub mod schema;
+
+use failure::{err_msg, Error};
+
+pub fn map_result_vec<T>(input: Vec<Result<T, Error>>) -> Result<Vec<T>, Error> {
+    let mut output: Vec<T> = Vec::new();
+    let mut errors: Vec<_> = Vec::new();
+    for item in input {
+        match item {
+            Ok(i) => output.push(i),
+            Err(e) => errors.push(format!("{}", e)),
+        }
+    }
+    if !errors.is_empty() {
+        Err(err_msg(errors.join("\n")))
+    } else {
+        Ok(output)
+    }
+}
