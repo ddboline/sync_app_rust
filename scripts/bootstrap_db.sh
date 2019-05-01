1cf0d6c0ef57b01625a113e46df8bf4df409fa2d
Daniel Boline <dboline@mediamath.com>
2019-05-01 13:50:00 -0400

    ...

--- scripts/bootstrap_db.sh
+++ scripts/bootstrap_db.sh
@@ -12,28 +12,6 @@ sudo -u postgres psql -c "CREATE ROLE $USER PASSWORD '$PASSWORD' NOSUPERUSER NOC
 sudo -u postgres psql -c "ALTER ROLE $USER PASSWORD '$PASSWORD' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"
 sudo -u postgres createdb $DB
 
-for DIR in ${HOME}/.config/garmin_rust ${HOME}/.garmin_cache/run/gps_tracks \
-           ${HOME}/.garmin_cache/run/cache ${HOME}/.garmin_cache/run/summary_cache;
-do
-    mkdir -p $DIR;
-done
-
-cat > ${HOME}/.config/garmin_rust/config.env <<EOL
+cat > ${HOME}/.config/sync_app_rust/config.env <<EOL
 DATABASE_URL=postgresql://$USER:$PASSWORD@localhost:5432/$DB
-MAPS_API_KEY=$MAPS_API_KEY
-GPS_BUCKET=garmin_scripts_gps_files_ddboline
-CACHE_BUCKET=garmin-scripts-cache-ddboline
-HTTP_BUCKET=garmin-scripts-http-cache
-SUMMARY_BUCKET=garmin-scripts-summary-cache
-GPS_DIR=${HOME}/.garmin_cache/run/gps_tracks
-CACHE_DIR=${HOME}/.garmin_cache/run/cache
-SUMMARY_CACHE=${HOME}/.garmin_cache/run/summary_cache
-JWT_SECRET=$JWT_SECRET
-SECRET_KEY=$SECRET_KEY
-DOMAIN=$DOMAIN
-SPARKPOST_API_KEY=$SPARKPOST_API_KEY
-AUTHDB=postgresql://$USER:$PASSWORD@localhost:5432/$DB
 EOL
-
-psql $DB < ./scripts/garmin_corrections_laps.sql
-psql $DB < ./scripts/garmin_summary.sql
