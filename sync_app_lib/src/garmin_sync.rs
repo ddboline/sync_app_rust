601a19cf4718248a8ff8a6171403f2be56c535ef
Daniel Boline <ddboline@gmail.com>
2019-11-18 20:14:50 -0500

    garmin_sync

---
+++ sync_app_lib/src/garmin_sync.rs
@@ -0,0 +1,188 @@
+use chrono::{DateTime, Duration, Utc};
+use crossbeam_utils::thread;
+use failure::{err_msg, Error};
+use maplit::hashmap;
+use reqwest::header::HeaderMap;
+use reqwest::{Response, Url};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt::Debug;
+
+use super::config::Config;
+use super::reqwest_session::ReqwestSession;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Copy)]
+struct ScaleMeasurement {
+    pub datetime: DateTime<Utc>,
+    pub mass: f64,
+    pub fat_pct: f64,
+    pub water_pct: f64,
+    pub muscle_pct: f64,
+    pub bone_pct: f64,
+}
+
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+struct FitbitHeartRate {
+    pub datetime: DateTime<Utc>,
+    pub value: i32,
+}
+
+pub struct GarminSync {
+    session0: ReqwestSession,
+    session1: ReqwestSession,
+    config: Config,
+}
+
+impl GarminSync {
+    pub fn new(config: Config) -> Self {
+        Self {
+            session0: ReqwestSession::new(true),
+            session1: ReqwestSession::new(true),
+            config,
+        }
+    }
+
+    pub fn get_urls(&self) -> Result<(Url, Url), Error> {
+        let from_url: Url = self
+            .config
+            .garmin_from_url
+            .as_ref()
+            .ok_or_else(|| err_msg("No From URL"))?
+            .parse()?;
+        let to_url: Url = self
+            .config
+            .garmin_to_url
+            .as_ref()
+            .ok_or_else(|| err_msg("No To URL"))?
+            .parse()?;
+        Ok((from_url, to_url))
+    }
+
+    pub fn init(&self) -> Result<(), Error> {
+        let (from_url, to_url) = self.get_urls()?;
+        let user = self
+            .config
+            .garmin_username
+            .as_ref()
+            .ok_or_else(|| err_msg("No Username"))?;
+        let password = self
+            .config
+            .garmin_password
+            .as_ref()
+            .ok_or_else(|| err_msg("No Password"))?;
+
+        let data = hashmap! {
+            "email" => user.as_str(),
+            "password" => password.as_str(),
+        };
+
+        let url = from_url.join("api/auth")?;
+        let resp = self
+            .session0
+            .post(&url, HeaderMap::new(), &data)?
+            .error_for_status()?;
+        let _: Vec<_> = resp.cookies().collect();
+        let url = to_url.join("api/auth")?;
+        let resp = self
+            .session1
+            .post(&url, HeaderMap::new(), &data)?
+            .error_for_status()?;
+        let _: Vec<_> = resp.cookies().collect();
+        Ok(())
+    }
+
+    pub fn run_sync(&self) -> Result<Vec<String>, Error> {
+        self.init()?;
+        let mut output = Vec::new();
+        let results =
+            self.run_single_sync("garmin/scale_measurements", "measurements", |mut resp| {
+                let results: Vec<ScaleMeasurement> = resp.json()?;
+                output.push(format!("measurements {}", results.len()));
+                let results: HashMap<_, _> =
+                    results.into_iter().map(|val| (val.datetime, val)).collect();
+                Ok(results)
+            })?;
+        output.extend_from_slice(&results);
+
+        for i in 0..10 {
+            let date = (Utc::now() - Duration::days(i)).naive_local().date();
+            let url = format!("garmin/fitbit/heartrate_db?date={}", date);
+            output.push(format!("start update {}", url));
+            let results = self.run_single_sync(&url, "updates", |mut resp| {
+                let results: Vec<FitbitHeartRate> = resp.json()?;
+                output.push(format!("updates {} {}", i, results.len()));
+                let results: HashMap<_, _> =
+                    results.into_iter().map(|val| (val.datetime, val)).collect();
+                Ok(results)
+            })?;
+            output.extend_from_slice(&results);
+        }
+
+        Ok(output)
+    }
+
+    fn run_single_sync<T, F>(
+        &self,
+        path: &str,
+        js_prefix: &str,
+        mut transform: F,
+    ) -> Result<Vec<String>, Error>
+    where
+        T: Debug + Serialize,
+        F: FnMut(Response) -> Result<HashMap<DateTime<Utc>, T>, Error>,
+    {
+        let mut output = Vec::new();
+        let (from_url, to_url) = self.get_urls()?;
+
+        let url = from_url.join(path)?;
+        let measurements0: HashMap<DateTime<Utc>, T> =
+            transform(self.session0.get(&url, HeaderMap::new())?)?;
+        let url = to_url.join(path)?;
+        let measurements1: HashMap<DateTime<Utc>, T> =
+            transform(self.session1.get(&url, HeaderMap::new())?)?;
+
+        let measurements: Vec<_> = measurements0
+            .iter()
+            .filter(|(k, _)| !measurements1.contains_key(&k))
+            .map(|(_, v)| v)
+            .collect();
+        if measurements.len() > 0 {
+            if measurements.len() < 20 {
+                output.push(format!("{:?}", measurements));
+            } else {
+                output.push(format!("{}", measurements.len()));
+            }
+            let url = to_url.join(path)?;
+            for meas in measurements.chunks(100) {
+                let data = hashmap! {
+                    js_prefix => meas,
+                };
+                self.session1
+                    .post(&url, HeaderMap::new(), &data)?
+                    .error_for_status()?;
+            }
+        }
+        let measurements: Vec<_> = measurements1
+            .iter()
+            .filter(|(k, _)| !measurements0.contains_key(&k))
+            .map(|(_, v)| v)
+            .collect();
+        if measurements.len() > 0 {
+            if measurements.len() < 20 {
+                output.push(format!("{:?}", measurements));
+            } else {
+                output.push(format!("{}", measurements.len()));
+            }
+            let url = to_url.join(path)?;
+            for meas in measurements.chunks(100) {
+                let data = hashmap! {
+                    js_prefix => meas,
+                };
+                self.session0
+                    .post(&url, HeaderMap::new(), &data)?
+                    .error_for_status()?;
+            }
+        }
+        Ok(output)
+    }
+}
---
+++ sync_app_lib/src/reqwest_session.rs
@@ -0,0 +1,130 @@
+use failure::{err_msg, Error};
+use parking_lot::Mutex;
+use rand::distributions::{Distribution, Uniform};
+use rand::thread_rng;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+use reqwest::{Client, RedirectPolicy, Response, Url};
+use serde::Serialize;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(Debug)]
+struct ReqwestSessionInner {
+    client: Client,
+    headers: HeaderMap,
+}
+
+pub struct ReqwestSession {
+    client: Arc<Mutex<ReqwestSessionInner>>,
+}
+
+impl Default for ReqwestSession {
+    fn default() -> Self {
+        Self::new(true)
+    }
+}
+
+impl ReqwestSessionInner {
+    pub fn get(&mut self, url: Url, mut headers: HeaderMap) -> Result<Response, Error> {
+        for (k, v) in &self.headers {
+            headers.insert(k, v.into());
+        }
+        self.client
+            .get(url)
+            .headers(headers)
+            .send()
+            .map_err(err_msg)
+    }
+
+    pub fn post<T>(
+        &mut self,
+        url: Url,
+        mut headers: HeaderMap,
+        form: &HashMap<&str, T>,
+    ) -> Result<Response, Error>
+    where
+        T: Serialize,
+    {
+        for (k, v) in &self.headers {
+            headers.insert(k, v.into());
+        }
+        self.client
+            .post(url)
+            .headers(headers)
+            .json(form)
+            .send()
+            .map_err(err_msg)
+    }
+}
+
+impl ReqwestSession {
+    pub fn new(allow_redirects: bool) -> Self {
+        let redirect_policy = if allow_redirects {
+            RedirectPolicy::default()
+        } else {
+            RedirectPolicy::none()
+        };
+        ReqwestSession {
+            client: Arc::new(Mutex::new(ReqwestSessionInner {
+                client: Client::builder()
+                    .cookie_store(true)
+                    .redirect(redirect_policy)
+                    .build()
+                    .expect("Failed to build client"),
+                headers: HeaderMap::new(),
+            })),
+        }
+    }
+
+    fn exponential_retry<T, U>(&self, f: T) -> Result<U, Error>
+    where
+        T: Fn() -> Result<U, Error>,
+    {
+        let mut timeout: f64 = 1.0;
+        let mut rng = thread_rng();
+        let range = Uniform::from(0..1000);
+        loop {
+            let resp = f();
+            match resp {
+                Ok(x) => return Ok(x),
+                Err(e) => {
+                    sleep(Duration::from_millis((timeout * 1000.0) as u64));
+                    timeout *= 4.0 * f64::from(range.sample(&mut rng)) / 1000.0;
+                    if timeout >= 64.0 {
+                        return Err(err_msg(e));
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn get(&self, url: &Url, headers: HeaderMap) -> Result<Response, Error> {
+        self.exponential_retry(|| self.client.lock().get(url.clone(), headers.clone()))
+    }
+
+    pub fn post<T>(
+        &self,
+        url: &Url,
+        headers: HeaderMap,
+        form: &HashMap<&str, T>,
+    ) -> Result<Response, Error>
+    where
+        T: Serialize,
+    {
+        self.exponential_retry(|| self.client.lock().post(url.clone(), headers.clone(), form))
+    }
+
+    pub fn set_default_headers(&self, headers: HashMap<&str, &str>) -> Result<(), Error> {
+        headers
+            .into_iter()
+            .map(|(k, v)| {
+                let name: HeaderName = k.parse()?;
+                let val: HeaderValue = v.parse()?;
+                self.client.lock().headers.insert(name, val);
+                Ok(())
+            })
+            .collect()
+    }
+}
