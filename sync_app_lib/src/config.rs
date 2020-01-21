d9c14e97859c920d26dd928f8a8f8082844db010
Daniel Boline <ddboline@gmail.com>
2020-01-20 22:33:43 -0500

    refactor config

--- sync_app_lib/src/config.rs
+++ sync_app_lib/src/config.rs
@@ -23,58 +23,82 @@ pub struct ConfigInner {
 #[derive(Default, Debug, Clone)]
 pub struct Config(Arc<ConfigInner>);
 
+macro_rules! set_config_ok {
+    ($s:ident, $id:ident) => {
+        $s.$id = var(&stringify!($id).to_uppercase()).ok();
+    };
+}
+
+macro_rules! set_config_parse {
+    ($s:ident, $id:ident, $d:expr) => {
+        $s.$id = var(&stringify!($id).to_uppercase())
+            .ok()
+            .and_then(|x| x.parse().ok())
+            .unwrap_or_else(|| $d);
+    };
+}
+
+macro_rules! set_config_must {
+    ($s:ident, $id:ident) => {
+        $s.$id = var(&stringify!($id).to_uppercase())
+            .map_err(|e| format_err!("{} must be set: {}", stringify!($id).to_uppercase(), e))?;
+    };
+}
+
+macro_rules! set_config_default {
+    ($s:ident, $id:ident, $d:expr) => {
+        $s.$id = var(&stringify!($id).to_uppercase()).unwrap_or_else(|_| $d);
+    };
+}
+
 impl Config {
     pub fn new() -> Self {
         Self::default()
     }
 
     pub fn init_config() -> Result<Self, Error> {
-        let fname = "config.env";
+        let fname = Path::new("config.env");
+        let config_dir = dirs::config_dir().ok_or_else(|| format_err!("No CONFIG directory"))?;
+        let home_dir = dirs::home_dir().ok_or_else(|| format_err!("No HOME directory"))?;
+        let default_fname = config_dir.join("sync_app_rust").join("config.env");
 
-        let home_dir = var("HOME").map_err(|e| format_err!("No HOME directory {}", e))?;
-
-        let default_fname = format!("{}/.config/sync_app_rust/config.env", home_dir);
-
-        let env_file = if Path::new(fname).exists() {
-            fname.to_string()
+        let env_file = if fname.exists() {
+            fname
         } else {
-            default_fname
+            &default_fname
         };
 
         dotenv::dotenv().ok();
 
-        if Path::new(&env_file).exists() {
-            dotenv::from_path(&env_file).ok();
-        } else if Path::new("config.env").exists() {
-            dotenv::from_filename("config.env").ok();
+        if env_file.exists() {
+            dotenv::from_path(env_file).ok();
         }
 
-        let default_gdrive_secret =
-            format!("{}/.config/sync_app_rust/client_secrets.json", home_dir);
-        let default_gdrive_token_path = format!("{}/.gdrive", home_dir);
+        let default_gdrive_secret = config_dir.join("sync_app_rust").join("client_secrets.json");
+        let default_gdrive_token_path = home_dir.join(".gdrive");
 
-        let conf = ConfigInner {
-            database_url: var("DATABASE_URL")
-                .map_err(|e| format_err!("DATABASE_URL must be set {}", e))?,
-            gdrive_secret_file: var("GDRIVE_SECRET_FILE").unwrap_or_else(|_| default_gdrive_secret),
-            gdrive_token_path: var("GDRIVE_TOKEN_PATH")
-                .unwrap_or_else(|_| default_gdrive_token_path),
-            aws_region_name: var("AWS_REGION_NAME").unwrap_or_else(|_| "us-east-1".to_string()),
-            secret_key: var("SECRET_KEY").unwrap_or_else(|_| "0123".repeat(8)),
-            domain: var("DOMAIN").unwrap_or_else(|_| "localhost".to_string()),
-            port: var("PORT")
-                .ok()
-                .and_then(|p| p.parse().ok())
-                .unwrap_or(3084),
-            n_db_workers: var("N_DB_WORKERS")
-                .ok()
-                .and_then(|n| n.parse().ok())
-                .unwrap_or(2),
-            garmin_username: var("GARMIN_USERNAME").ok(),
-            garmin_password: var("GARMIN_PASSWORD").ok(),
-            garmin_from_url: var("GARMIN_FROM_URL").ok(),
-            garmin_to_url: var("GARMIN_TO_URL").ok(),
-        };
+        let mut conf = ConfigInner::default();
+
+        set_config_must!(conf, database_url);
+        set_config_default!(
+            conf,
+            gdrive_secret_file,
+            default_gdrive_secret.to_string_lossy().into()
+        );
+        set_config_default!(
+            conf,
+            gdrive_token_path,
+            default_gdrive_token_path.to_string_lossy().into()
+        );
+        set_config_default!(conf, aws_region_name, "us-east-1".to_string());
+        set_config_default!(conf, secret_key, "0123".repeat(8));
+        set_config_default!(conf, domain, "localhost".to_string());
+        set_config_parse!(conf, port, 3084);
+        set_config_parse!(conf, n_db_workers, 2);
+        set_config_ok!(conf, garmin_username);
+        set_config_ok!(conf, garmin_password);
+        set_config_ok!(conf, garmin_from_url);
+        set_config_ok!(conf, garmin_to_url);
 
         Ok(Self(Arc::new(conf)))
     }
