dbc07107038f95ac3c58546786aa883c02bec2e8
Daniel Boline <ddboline@gmail.com>
2020-02-03 10:58:56 -0500

    fix test

--- sync_app_lib/src/file_list_local.rs
+++ sync_app_lib/src/file_list_local.rs
@@ -318,7 +318,7 @@ mod tests {
             cache_info.md5sum.as_ref().unwrap()
         );
 
-        let cache_info = FileInfoCache::from_insert(cache_info, 5);
+        let cache_info = &FileInfoCache::from_insert(cache_info, 5);
         let test_result: FileInfo = cache_info.try_into()?;
         assert_eq!(*result, test_result);
 
