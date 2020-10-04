86b07db26b8ce691825a0f436b12c54baaf94d64
Daniel Boline <ddboline@gmail.com>
2020-10-03 21:22:14 -0400

    stop syncing movie_queue, version bump

--- sync_app_lib/src/movie_sync.rs
+++ sync_app_lib/src/movie_sync.rs
@@ -98,7 +98,7 @@ impl MovieSync {
         sync_single_table!("imdb_ratings", "shows", ImdbRatings);
         sync_single_table!("imdb_episodes", "episodes", ImdbEpisodes);
         sync_single_table!("movie_collection", "collection", MovieCollectionRow);
-        sync_single_table!("movie_queue", "queue", MovieQueueRow);
+        // sync_single_table!("movie_queue", "queue", MovieQueueRow);
 
         Ok(output)
     }
