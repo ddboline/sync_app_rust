6fadcaaeb75b4043e451efb7876fc859756b6b3d
Daniel Boline <ddboline@gmail.com>
2021-01-11 08:54:19 -0500

    version bumps (#7)
    uprade to tokio 1.0

--- sync_app_http/src/errors.rs
+++ sync_app_http/src/errors.rs
@@ -1,4 +1,3 @@
-use actix_threadpool::BlockingError;
 use actix_web::{error::ResponseError, HttpResponse};
 use anyhow::Error as AnyhowError;
 use std::fmt::Debug;
@@ -18,8 +17,6 @@ pub enum ServiceError {
     Unauthorized,
     #[error("Anyhow error {0}")]
     AnyhowError(#[from] AnyhowError),
-    #[error("blocking error {0}")]
-    BlockingError(StackString),
 }
 
 // impl ResponseError trait allows to convert our errors into http responses
@@ -36,12 +33,6 @@ impl ResponseError for ServiceError {
                 HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
             }
         }
-    }
-}
-
-impl<T: Debug> From<BlockingError<T>> for ServiceError {
-    fn from(item: BlockingError<T>) -> Self {
-        Self::BlockingError(format!("{:?}", item).into())
     }
 }
 
