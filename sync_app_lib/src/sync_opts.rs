4fe50e5d8eee24aafeaafa3ca92b716bd3a9cff1
Daniel Boline <ddboline@gmail.com>
2021-05-16 17:49:42 -0400

    clippy

--- sync_app_lib/src/sync_opts.rs
+++ sync_app_lib/src/sync_opts.rs
@@ -121,7 +121,7 @@ impl SyncOpts {
 
                     FileSyncConfig::get_url_list(&pool).await?
                 } else {
-                    self.urls.to_vec()
+                    self.urls.clone()
                 };
                 let futures = urls.into_iter().map(|url| {
                     let blacklist = blacklist.clone();
